8dc118b3d25a07b11ebaa78cc7ab0fdaa1ae98f4
asuarezgarcia <a.suarezg@uniandes.edu.co>
2024-12-05 09:33:08 -0500

    a

--- app/app/src/main/java/uniandes/edu/co/app/repositorio/SucursalRepository.java
+++
@@ -1,9 +0,0 @@
-package uniandes.edu.co.app.repositorio;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import uniandes.edu.co.app.modelo.Sucursal;
-
-public interface SucursalRepository extends MongoRepository<Sucursal, Integer> {
-    
-}
